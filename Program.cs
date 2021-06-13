49a3310a82e179e5c8562157e37d38d608d4b5e6
Ectario <dimitritristangalaxy@gmail.com>
2021-06-13 19:40:04 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -7,20 +7,10 @@ namespace Reply21
         static void Main(string[] args)
         {
 
-            //Test output
-            Console.WriteLine("Text :\n");
             Input input = new Input();
-
             Output output = new Output();
 
-            Grid grid = input.GetGrid("");
-
-            grid.lstAntenna[0].x = 6;
-            grid.lstAntenna[0].y = 19;
-
-            grid.lstAntenna.Add(new Antenna(1, 12, 34));
-            grid.lstAntenna.Add(new Antenna(2, 32, 14));
-            grid.lstAntenna.Add(new Antenna(3, 2, 4));
+            Grid grid = input.GetGrid("./scenario/a_example.in");
 
             int cptBuilding = 0;
             foreach (Building building in grid.lstBuildings)
@@ -34,12 +24,8 @@ namespace Reply21
                 else break;
             }
 
-
-            grid.file = "";
             string pathOutput = output.generateOutput(grid);
-
             Console.WriteLine("Ouput generated in file : " + pathOutput);
-            //End test output
 
         }
     }
